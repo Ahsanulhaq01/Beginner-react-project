abfe86816da0a93a92308bc31ed9c1f051289cae
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-08-14 19:22:56 +0500

    setup the code and add alert to signup btn on submit

--- form-validation-app/src/FormValidation.jsx
+++ form-validation-app/src/FormValidation.jsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function FormValidation() {
   const [seePass, setSeePass] = useState(true);
   const [formData, setFormData] = useState({});
   const [error, setError] = useState({});
-  const refSubmitbtn = useRef(null)
 
   function handleSeeIcon() {
     setSeePass(seePass ? false : true);
@@ -16,17 +15,17 @@ function FormValidation() {
     //   ...preData ,
     //   name : inputName,
     // }))
-   
-    setFormData({name : inputName})
+
+    setFormData({ name: inputName });
     if (inputName.trim() === "") {
       setError({ name: "name is required" });
-      event.target.style.border = '2px solid red';
+      event.target.style.border = "2px solid red";
     } else if (inputName.trim().length < 3) {
       setError({ name: "Name must be at least of three letter" });
-       event.target.style.border = '2px solid red';
+      event.target.style.border = "2px solid red";
     } else {
       setError({ name: "" });
-      event.target.style.border = '2px solid green';
+      event.target.style.border = "2px solid green";
     }
   }
   function handleEmailInput(event) {
@@ -37,13 +36,13 @@ function FormValidation() {
     }));
     if (inputEmail.trim() === "") {
       setError({ email: "Email is required" });
-      event.target.style.border = '2px solid red'
+      event.target.style.border = "2px solid red";
     } else if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
       setError({ email: "Enter the valid email" });
-      event.target.style.border = '2px solid red'
+      event.target.style.border = "2px solid red";
     } else {
       setError({ email: "" });
-      event.target.style.border = '2px solid green';
+      event.target.style.border = "2px solid green";
     }
   }
   function handlePasswordInput(event) {
@@ -54,13 +53,13 @@ function FormValidation() {
     }));
     if (passInput.trim() === "") {
       setError({ pass: "Password is required " });
-      event.target.style.border = '2px solid red'
-    } else if (passInput.length !== 8) {
+      event.target.style.border = "2px solid red";
+    } else if (passInput.length < 8) {
       setError({ pass: "Password must be of 8 character" });
-      event.target.style.border = '2px solid red'
+      event.target.style.border = "2px solid red";
     } else {
       setError({ pass: "" });
-      event.target.style.border = '2px solid green';
+      event.target.style.border = "2px solid green";
     }
   }
   function handleConfirmPassInput(event) {
@@ -72,89 +71,94 @@ function FormValidation() {
 
     if (inputConfirmPass.trim() === "") {
       setError({ confirmPass: "confirm password is required" });
-      event.target.style.border = '2px solid red'
+      event.target.style.border = "2px solid red";
     } else if (inputConfirmPass !== formData.pass) {
       setError({ confirmPass: "Pasword must be same " });
-      event.target.style.border = '2px solid red'
+      event.target.style.border = "2px solid red";
     } else {
       setError({ confirmPass: "" });
-      event.target.style.border = '2px solid green';
+      event.target.style.border = "2px solid green";
     }
   }
-  function handleSubmit() {
-    if(!error){
-      console.log(refSubmitbtn.current)
-    }
+  function handleSubmit(event){
+    event.preventDefault();
+    alert('form successfully submitted')
   }
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <div className="app-container">
-        <h1 className="heading">Sign Up</h1>
-        <div className="name-container containers">
-          <label htmlFor="name-input">Name :</label>
-          <input
-            type="text"
-            placeholder="Enter your name "
-            id="name-input"
-            required
-            className="input-field"
-            onChange={handleNameInput}
-          />
-          {error.name && <p className="name-error error">{error.name}</p>}
-        </div>
-        <div className="email-container containers">
-          <label htmlFor="email-input">Email :</label>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            required
-            id="email-input"
-            className="input-field"
-            onChange={handleEmailInput}
-          />
-          {error.email && <p className="email-error error">{error.email}</p>}
-        </div>
-        <div className="password-container containers">
-          <label htmlFor="password-input">Password</label>
-          <div className="input-div">
+      <form onSubmit={handleSubmit} >
+        <div className="app-container">
+          <h1 className="heading">Sign Up</h1>
+          <div className="name-container containers">
+            <label htmlFor="name-input">Name :</label>
+            <input
+              type="text"
+              placeholder="Enter your name "
+              id="name-input"
+              required
+              className="input-field"
+              onChange={handleNameInput}
+            />
+            {error.name && <p className="name-error error">{error.name}</p>}
+          </div>
+          <div className="email-container containers">
+            <label htmlFor="email-input">Email :</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              required
+              id="email-input"
+              className="input-field"
+              onChange={handleEmailInput}
+            />
+            {error.email && <p className="email-error error">{error.email}</p>}
+          </div>
+          <div className="password-container containers">
+            <label htmlFor="password-input">Password</label>
+            <div className="input-div">
+              <input
+                type={seePass ? "password" : "text"}
+                placeholder="Enter password"
+                id="password-input"
+                className="input-field"
+                onChange={handlePasswordInput}
+              />
+              <button onClick={handleSeeIcon} className="see-pass-btn">
+                {seePass ? (
+                  <i className="fa fa-eye"></i>
+                ) : (
+                  <i className="fa fa-eye-slash"></i>
+                )}
+              </button>
+            </div>
+            {error.pass && <p className="pass-error error">{error.pass}</p>}
+          </div>
+          <div className="confirm-password-container containers">
+            <label htmlFor="confirm-password-input">Confirm Password</label>
             <input
               type={seePass ? "password" : "text"}
               placeholder="Enter password"
-              id="password-input"
+              id="confirm-password-input"
               className="input-field"
-              onChange={handlePasswordInput}
+              onChange={handleConfirmPassInput}
             />
-            <button onClick={handleSeeIcon} className="see-pass-btn">
-              {seePass ? (
-                <i className="fa fa-eye"></i>
-              ) : (
-                <i className="fa fa-eye-slash"></i>
-              )}
+            {error.confirmPass && (
+              <p className="pass-error error">{error.confirmPass}</p>
+            )}
+          </div>
+          <div className="button-container">
+            <button
+              className="sign-up-btn"
+              type="submit"
+              // disabled={!!(error.name||error.email||error.pass||error.confirmPass )}
+              disabled = {Object.values(error).some(err => err !== '')}
+
+            >
+              Sign Up
             </button>
           </div>
-          {error.pass && <p className="pass-error error">{error.pass}</p>}
         </div>
-        <div className="confirm-password-container containers">
-          <label htmlFor="confirm-password-input">Confirm Password</label>
-          <input
-            type={seePass ? "password" : "text"}
-            placeholder="Enter password"
-            id="confirm-password-input"
-            className="input-field"
-            onChange={handleConfirmPassInput}
-          />
-          {error.confirmPass && (
-            <p className="pass-error error">{error.confirmPass}</p>
-          )}
-        </div>
-        <div className="button-container">
-          <button className="sign-up-btn" type="submit" ref={refSubmitbtn}>
-            Sign Up
-          </button>
-        </div>
-      </div>
       </form>
     </>
   );
